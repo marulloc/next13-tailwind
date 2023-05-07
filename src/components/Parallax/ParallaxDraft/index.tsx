f9a7ed14d8b0351e6f8aecdcde7db6832730934c
marulloc <marullo@naver.com>
2023-05-07 18:49:28 +0900

    parallax birds and sun

--- src/components/Parallax/ParallaxDraft/index.tsx
+++ src/components/Parallax/ParallaxDraft/index.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styles from './parallax_draft.module.css';
 import Image from 'next/image';
 import { ParallaxBG } from '@/components/svgs';
@@ -12,8 +12,34 @@ const ParallaxDraft = () => {
 
     const handleBtnClick = (e: any) => setActive(e.target.id);
 
+    const svgRef = useRef<any>(null);
+
+    useEffect(() => {
+        const svg = svgRef.current;
+        if (!svg) return;
+
+        const handleScroll = () => {
+            const sun = svg.querySelector('#parallax_background_svg__sun');
+            const grayClouds = svg.querySelector('#parallax_background_svg__gray_clouds');
+            const whiteClouds = svg.querySelector('#parallax_background_svg__white_clouds');
+            const bird1 = svg.querySelector('#parallax_background_svg__bird1');
+            const bird2 = svg.querySelector('#parallax_background_svg__bird2');
+            const bird3 = svg.querySelector('#parallax_background_svg__bird3');
+
+            sun.style['transform'] = `translateY(${scrollY * 1.05}px)`;
+            grayClouds.style['transform'] = `translateX(${scrollY * 0.125}px)`;
+            whiteClouds.style['transform'] = `translateX(${scrollY * -0.125}px)`;
+            bird1.style['transform'] = `translateX(${scrollY}px)`;
+            bird2.style['transform'] = `translateX(${scrollY * -3.5}px)`;
+            bird3.style['transform'] = `translateX(${scrollY * -0.5}px)`;
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
     return (
-        <>
+        <main style={{ height: '200vh' }}>
             <header className={styles['header']}>
                 <a
                     id="btn-1"
@@ -49,11 +75,11 @@ const ParallaxDraft = () => {
             </header>
 
             <div className={styles['image-wrapper']}>
-                <div className={styles['image-box']}>
+                <div ref={svgRef} className={styles['image-box']}>
                     <ParallaxBG />
                 </div>
             </div>
-        </>
+        </main>
     );
 };
 
