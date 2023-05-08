f1a8788f780037277f0be80cdb064db88e75ca27
marulloc <marullo@naver.com>
2023-05-08 21:27:05 +0900

    (draft) locale middleware

---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+import { NextMiddleware, NextRequest, NextResponse } from 'next/server';
+
+let locales = ['en-US', 'nl-NL', 'nl'];
+
+const getLocale = (request: NextRequest) => {
+    return locales[0];
+};
+
+export const middleware: NextMiddleware = async (request) => {
+    // Check if there is any supported locale in the pathname
+    const pathname = request.nextUrl.pathname;
+
+    const pathnameIsMissingLocale = locales.every(
+        (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`,
+    );
+
+    // Redirect if there is no locale
+    if (pathnameIsMissingLocale) {
+        console.log(pathname);
+        const locale = getLocale(request);
+
+        // e.g. incoming request is /products
+        // The new URL is now /en-US/products
+        return NextResponse.redirect(new URL(`/${locale}/${pathname}`, request.url));
+    }
+};
+
+export const config = {
+    matcher: ['/((?!api|_next/static|_next/image|favicon.ico|next.svg|vercel.svg).*)'],
+};
